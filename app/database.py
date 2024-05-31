ea192cbf055e582c748536147d5dce10ffedddc5
yokesh492 <yokeshkummar@gmail.com>
2024-05-31 23:11:25 +0530

    config: production DB

--- app/database.py
+++ app/database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# PostgreSQL connection string
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Pokemon492#@localhost/Resource"
+# PostgreSQL connection string local
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Pokemon492#@localhost/Resource"
+
+#for production
+SQLALCHEMY_DATABASE_URL = "postgresql://yoke492:Pokemon492#@91.108.104.64:5432/mydatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
