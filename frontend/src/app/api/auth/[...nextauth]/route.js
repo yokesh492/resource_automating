c59adc55847197dd1c89a2e00415dd869cafc98f
Sivaneshan-04 <ed21b062@smail.iitm.ac.in>
2024-06-18 13:38:49 +0530

    Checking signin on deployment

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -14,20 +14,20 @@ const authOptions = {
     async signIn(user, account, profile) {
         console.log('Querying signin')
         try{
-            // const resp = await axios.post(`${process.env.NEXT_PUBLIC_PRODUCTION}/signup_with_google`, {
-            //     email: user.email,
-            //     username: user.name,
-            // });
+            const resp = await axios.post(`${process.env.NEXT_PUBLIC_PRODUCTION}/signup_with_google`, {
+                email: user.email,
+                username: user.name,
+            });
             
-            // console.log(resp.data,'response from backend');
+            console.log(resp.data,'response from backend');
     
-            const resp = {data:{userid:1, username:'test', error:null,image:'https://unsplash.com/photos/woman-with-dslr-camera-e616t35Vbeg'}, status:200};
+            // const resp = {data:{userid:1, username:'test', error:null,image:'https://unsplash.com/photos/woman-with-dslr-camera-e616t35Vbeg'}, status:200};
     
             if (resp.data.error || resp.status !== 200) {
                 console.log('error in signin');
                 return false;
             }
-            
+
             console.log('Setting cookies')
             await setUserCookies('userinfo', {userid:resp.data.userid, username:resp.data.username});
             await setJWTCookies('session', {userid:resp.data.userid, username:resp.data.username});
